b8496d0f8880fff96f7e0b095fed3334335afacd
shihab-2019331068 <siababdullah3946@gmail.com>
2025-10-24 18:47:48 +0600

    feat(projection): add solution for projection challenge

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,30 +1,46 @@
+import { NgOptimizedImage } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
 import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
     <app-card
       [list]="students()"
-      [type]="cardType"
-      customClass="bg-light-green" />
+      class="bg-light-green"
+      [itemTemplate]="itemTemplate"
+      (onAddNewItem)="addNewItem()">
+      <img ngSrc="assets/img/student.webp" width="200" height="200" />
+    </app-card>
+
+    <ng-template #itemTemplate let-item>
+      <app-list-item
+        [name]="item.firstName"
+        [id]="item.id"
+        [type]="cardType"
+        (onDeleteItem)="deleteStudent(item.id)"></app-list-item>
+    </ng-template>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-green {
+      .bg-light-green {
         background-color: rgba(0, 250, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardComponent, NgOptimizedImage, ListItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentCardComponent implements OnInit {
@@ -36,5 +52,13 @@ export class StudentCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+  }
+
+  addNewItem() {
+    this.store.addOne(randStudent());
+  }
+
+  deleteStudent(id: number) {
+    this.store.deleteOne(id);
   }
 }
--- apps/angular/1-projection/src/app/data-access/fake-http.service.ts
+++ apps/angular/1-projection/src/app/data-access/fake-http.service.ts
@@ -56,7 +56,7 @@ export const randomCity = (): City => ({
   country: randCountry(),
 });
 
-const cities = [randomCity(), randomCity(), randomCity()];
+const cities: City[] = [randomCity(), randomCity(), randomCity()];
 
 @Injectable({
   providedIn: 'root',
